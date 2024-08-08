4d8efe6097ab52a2c32f971005a5e576bda11c67
Yrving <yrving.molina@mo2o.com>
2024-08-08 11:09:21 -0500

    Initial commit

---
+++ IndTourismApp/IndTourismApp/IndTourismAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndTourismAppApp.swift
+//  IndTourismApp
+//
+//  Created by Yrving Heysen Molina Yriarte on 8/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IndTourismAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
